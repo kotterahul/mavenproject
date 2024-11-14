d81879cd195b1b3b4e0471130005c443d210943c
kotterhl@gmail.com <kotterhl@gmail.com>
2024-11-14 20:38:11 +0530

    File commited

---
+++ src/main/java/com/app/demo/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Rahul -- 22BD1A054Z" );
+    }
+}
