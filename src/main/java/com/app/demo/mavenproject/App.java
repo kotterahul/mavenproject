9b0811dd859e4e8ca4ae28d622b49999ac2f5e73
kotterahul <147540364+kotterahul@users.noreply.github.com>
2024-11-25 14:52:44 +0530

    Update App.java

--- src/main/java/com/app/demo/mavenproject/App.java
+++ src/main/java/com/app/demo/mavenproject/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Rahul -- 22BD1A054Z" );
+        System.out.println("This is line 1");
+        System.out.println("This is line 2")
     }
 }
